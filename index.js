4f10d6efe61137be541d167ac890175fef965058
Alejo2589 <asrivera1989@gmail.com>
2023-07-08 21:45:54 -0500

    Versión 0.1

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    this.document.getElementById("verAlerta").addEventListener("click", function () {
+        alert("Gracias por la visita");
+    })
+})
+
